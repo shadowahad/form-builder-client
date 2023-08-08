551a7f060f4da82831a4a1cc27ffca1aade58e69
shadowahad <abdulahaad.tahir@ascend.com.sa>
2023-08-08 18:17:47 +0500

    scss config added

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import scss from "rollup-plugin-scss";
 
 const packageJson = require("./package.json");
 
@@ -26,6 +27,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      scss(),
     ],
     external: ["react", "react-dom"],
   },
